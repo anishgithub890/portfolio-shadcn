b2fbd35b631b9c1377bfde030f7a7fc15d68b0b2
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-11-12 03:06:40 +0545

    testimonial:page-completed,client-ongoing

---
+++ app/(admin)/admintestimonials/components/client.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Plus } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
+import { HeadingTheme } from '@/components/ui/heading-theme';
+import { Separator } from '@/components/ui/separator';
+import { ApiList } from '@/components/ui/api-list';
+
+import { columns, TestimonialColumn } from './columns';
+
+interface TestimonialClientProps {
+  data: TestimonialColumn[];
+}
+
+export const TestimonialClient: React.FC<TestimonialClientProps> = ({
+  data,
+}) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <HeadingTheme
+          title={`Testimonials (${data.length})`}
+          description="Manage testimonial for your portfolio"
+        />
+        <Button onClick={() => router.push(`/admintestimonials/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+    </>
+  );
+};
---
+++ app/(admin)/admintestimonials/page.tsx
@@ -0,0 +1,57 @@
+import { format } from 'date-fns';
+
+import prisma from '@/lib/prismadb';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+import { TestimonialColumn } from './components/columns';
+import { TestimonialClient } from './components/client';
+import Container from '@/components/container';
+import RoleState from '@/components/role-state';
+
+const AdminTestimonialsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <RoleState title="Unauthorized" description="Please login" />;
+  }
+
+  const testimonials = await prisma.testimonial.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  const formattedTestimonials: TestimonialColumn[] = testimonials.map(
+    (item) => ({
+      id: item.id,
+      name: item.name,
+      role: item.role,
+      createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    })
+  );
+
+  return (
+    <div className="flex-col pt-14">
+      <Container>
+        {currentUser?.role == 'user' ? (
+          <div>
+            <RoleState
+              title="OOOPS! ACCESS DENIED"
+              description="This page not for users....!!"
+            />
+          </div>
+        ) : currentUser?.role == 'admin' ? (
+          <>
+            <div className="flex-1 space-y-4 p-8 pt-6">
+              <TestimonialClient data={formattedTestimonials} />
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default AdminTestimonialsPage;
