c016a09eb4bde6d540455e1e11f39f3a8c33293f
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-11-13 02:45:18 +0545

    admin:testimonial-form:-completed

--- app/(admin)/admintestimonials/[admintestimonialId]/components/testimonial-form.tsx
+++ app/(admin)/admintestimonials/[admintestimonialId]/components/testimonial-form.tsx
@@ -27,6 +27,7 @@ import ImageUpload from '@/components/ui/image-upload';
 import { Preview } from '@/components/preview';
 import { Textarea } from '@/components/ui/textarea';
 import { Editor } from '@/components/editor';
+import { url } from 'inspector';
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -58,5 +59,186 @@ interface TestimonialFormProps {
 export const TestimonialForm: React.FC<TestimonialFormProps> = ({
   initialData,
 }) => {
-  return <>Hello Testimonial</>;
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? 'Edit testimonial' : 'Create testimonial';
+  const description = initialData
+    ? 'Edit a testimonial.'
+    : 'Add a new testimonial';
+  const toastMessage = initialData
+    ? 'Testimonial updated.'
+    : 'Testimonial created.';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  const form = useForm<TestimonialFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      role: '',
+      company: '',
+      comment: '',
+      imageUrl: '',
+    },
+  });
+
+  const onSubmit = async (data: TestimonialFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/testimonials/${params?.admintestimonialId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/testimonials`, data);
+      }
+      router.refresh();
+      router.push(`/admintestimonials`);
+      toast.success(toastMessage);
+    } catch (error: any) {
+      toast.error('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/testimonials/${params?.admintestimonialId}`);
+      router.refresh();
+      router.push(`/admintestimonials`);
+      toast.success('Testimonial deleted.');
+    } catch (error: any) {
+      toast.error('Make sure you removed all testimonial.');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+
+      <div className="flex items-center justify-between">
+        <HeadingTheme title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Reviewer image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange('')}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="md:grid md:grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Reviewer Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="testimonial name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Reviewer Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="testimonial role"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="company"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Reviewer Company</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="testimonial company"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="comment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Reviewer Description</FormLabel>
+                  <FormControl>
+                    <Editor {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
 };
