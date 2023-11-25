cb2d0c231e0966c2aecc38bfd530d51a439b536b
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-11-26 01:51:04 +0545

    actions:getProjectById.ts

---
+++ app/actions/getProjectById.ts
@@ -0,0 +1,42 @@
+import prisma from '@/lib/prismadb';
+
+interface IParams {
+  projectId: string;
+}
+
+export default async function getProjectById(params: IParams) {
+  try {
+    const { projectId } = params;
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!project) {
+      return null;
+    }
+
+    return {
+      ...project,
+
+      createdAt: project.createdAt.toISOString(),
+      updatedAt: project.updatedAt.toISOString(),
+
+      user: {
+        ...project.user,
+
+        createdAt: project.user.createdAt.toISOString(),
+        updatedAt: project.user.updatedAt.toISOString(),
+        emailVerified: project.user.emailVerified?.toISOString() || null,
+      },
+    };
+  } catch (error) {
+    console.log('[get_PROJECTBYID]');
+    return null;
+  }
+}
