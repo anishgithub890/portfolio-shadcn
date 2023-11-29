1e570b277feeedc08e4f931a0a4eb30987d5567a
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-11-29 22:47:24 +0545

    adminresume:file-set-up

--- app/(client)/(routes)/screen-cards/experience-card.tsx
+++ app/(client)/(routes)/screen-cards/experience-card.tsx
@@ -2,7 +2,7 @@
 
 import { SafeExperience, SafeUser } from '@/app/types';
 import { Preview } from '@/components/preview';
-import { cn } from '@/lib/utils';
+import { Badge } from '@/components/ui/badge';
 
 interface ExperienceCardProps {
   data: SafeExperience;
@@ -15,7 +15,11 @@ const ExperienceCard: React.FC<ExperienceCardProps> = ({ data }) => {
       <figure className="md:flex bg-slate-100 hover:bg-slate-200 transition rounded-xl p-8 md:p-0">
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <figcaption className="font-medium">
-            <div className="text-sky-500">{data.year}</div>
+            <Badge variant="outline">
+              <div className="text-sky-500 text-sm font-medium">
+                {data.year}
+              </div>
+            </Badge>
             <div className="text-slate-700">{data.language}</div>
           </figcaption>
           <blockquote>
