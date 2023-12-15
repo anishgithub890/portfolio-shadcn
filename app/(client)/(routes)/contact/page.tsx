4b310bb582e30aaed2abf17c093339fdff3174ea
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-12-15 18:05:47 +0545

    contactpage:-rounded-flex-fixed

--- app/(client)/(routes)/contact/page.tsx
+++ app/(client)/(routes)/contact/page.tsx
@@ -24,30 +24,32 @@ const ContactPage = () => {
             <div className="md:flex bg-slate-100 hover:bg-slate-200 transition rounded-md p-8 md:p-0">
               <div className="md:p-8 text-center md:text-left space-y-4">
                 <span className="flex gap-4 dark:text-zinc-900">
-                  <div className="rounded-xl bg-slate-300 p-4">
+                  <div className="fixed rounded-xl bg-slate-300 p-4">
                     <Phone className="w-6 h-6" />
                   </div>
-                  <p className="p-4 break-all">+977-9845695512</p>
+                  <p className="p-4 break-all pl-[5rem]">+977-9845695512</p>
                 </span>
               </div>
             </div>
             <div className="md:flex bg-slate-100 hover:bg-slate-200 transition rounded-md p-8 md:p-0">
               <div className="md:p-8 text-center md:text-left space-y-4">
                 <span className="flex gap-4 dark:text-zinc-900">
-                  <div className="rounded-xl bg-slate-300 p-4">
+                  <div className="fixed rounded-xl bg-slate-300 p-4">
                     <Mail className="w-6 h-6" />
                   </div>
-                  <p className="p-4 break-all">anishgithub890@gmail.com</p>
+                  <p className="p-4 break-all pl-[5rem]">
+                    anishgithub890@gmail.com
+                  </p>
                 </span>
               </div>
             </div>
             <div className="md:flex bg-slate-100 hover:bg-slate-200 transition rounded-md p-8 md:p-0">
               <div className="md:p-8 text-center md:text-left space-y-4">
                 <span className="flex gap-4 dark:text-zinc-900">
-                  <div className="flex rounded-xl bg-slate-300 p-4">
+                  <div className="fixed rounded-xl bg-slate-300 p-4">
                     <MapPin className="w-6 h-6" />
                   </div>
-                  <p className="p-4 break-all">Kathmandu, Nepal</p>
+                  <p className="p-4 break-all pl-[5rem]">Kathmandu, Nepal</p>
                 </span>
               </div>
             </div>
