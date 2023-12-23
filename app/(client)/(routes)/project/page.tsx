e43062566847b7fb3410cd01853214304678ae6d
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-12-23 18:05:26 +0545

    rm-getprojectimage,project-card:almost-end

--- app/(client)/(routes)/project/page.tsx
+++ app/(client)/(routes)/project/page.tsx
@@ -1,57 +1,55 @@
-// import getCurrentUser from '@/app/actions/getCurrentUser';
-// import getProjects from '@/app/actions/getProjects';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getProjects from '@/app/actions/getProjects';
 
-// import ClientOnly from '@/components/client-only';
-// import Container from '@/components/container';
-// import EmptyState from '@/components/empty-state';
-// import ProjectCard from '../screen-cards/project-card';
-// import geProjectImages from '@/app/actions/getProjectImages';
+import ClientOnly from '@/components/client-only';
+import Container from '@/components/container';
+import EmptyState from '@/components/empty-state';
+import ProjectCard from '../screen-cards/project-card';
 
-// const ProjectPage = async () => {
-//   const currentUser = await getCurrentUser();
+const ProjectPage = async () => {
+  const currentUser = await getCurrentUser();
 
-//   const projects = await getProjects({ isFeatured: true });
-//   const images = await geProjectImages({ isFeatured: true });
+  const projects = await getProjects({ isFeatured: true });
 
-//   return (
-//     <>
-//       <ClientOnly>
-//         <Container>
-//           <div className="pt-14">
-//             {projects?.length === 0 ? (
-//               <div className="pt-1">
-//                 <EmptyState />
-//               </div>
-//             ) : (
-//               <div
-//                 className="
-//                 pt-8
-//                 grid
-//                 grid-cols-1
-//                 sm:grid-cols-1
-//                 md:grid-cols-2
-//                 lg:grid-cols-3
-//                 xl:grid-cols-3
-//                 2xl:grid-cols-3
-//                 gap-4
-//               "
-//               >
-//                 {projects?.map((project: any) => {
-//                   return (
-//                     <ProjectList
-//                       title="Featured Products"
-//                       items={projects}
-//                       key={project.id}
-//                     />
-//                   );
-//                 })}
-//               </div>
-//             )}
-//           </div>
-//         </Container>
-//       </ClientOnly>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <ClientOnly>
+        <Container>
+          <div className="pt-14">
+            {projects?.length === 0 ? (
+              <div className="pt-1">
+                <EmptyState />
+              </div>
+            ) : (
+              <div
+                className="
+                pt-8
+                grid
+                grid-cols-1
+                sm:grid-cols-1
+                md:grid-cols-2
+                lg:grid-cols-3
+                xl:grid-cols-3
+                2xl:grid-cols-3
+                gap-4
+              "
+              >
+                {projects?.map((project: any) => {
+                  return (
+                    <ProjectCard
+                      currentUser={currentUser}
+                      key={project.id}
+                      data={project}
+                    />
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </Container>
+      </ClientOnly>
+    </>
+  );
+};
 
-// export default ProjectPage;
+export default ProjectPage;
--- app/(client)/(routes)/screen-cards/project-card.tsx
+++ app/(client)/(routes)/screen-cards/project-card.tsx
@@ -2,11 +2,16 @@
 
 import { useRouter } from 'next/navigation';
 
-import { SafeProject } from '@/app/types';
+import { SafeProject, SafeUser } from '@/app/types';
 import { Preview } from '@/components/preview';
+import Image from 'next/image';
+import { Separator } from '@/components/ui/separator';
+import Link from 'next/link';
+import { ExternalLink, Github } from 'lucide-react';
 
 interface ProjectCardProps {
   data: SafeProject;
+  currentUser?: SafeUser | null;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
@@ -18,10 +23,40 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
 
   return (
     <>
-      <div className="md:flex bg-slate-100 hover:bg-slate-200 transition rounded-xl p-8 md:p-0">
+      <div className="md:flex bg-slate-100 hover:bg-slate-200/75 transition rounded-xl p-8 md:p-0">
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <div className="flex items-center font-medium dark:text-zinc-700">
-            <Preview value={data.explanation} />
+          <div onClick={handleClick} className="items-center">
+            <Image
+              src={data.imageUrl}
+              alt=""
+              width={`900`}
+              height={`900`}
+              className="rounded-md cursor-pointer"
+            />
+          </div>
+          <div className="flex-col items-center font-medium dark:text-zinc-700">
+            <Preview value={data.explanation.substring(0, 400)} />
+            <strong
+              onClick={handleClick}
+              className="text-rose-700 pl-4 text-sm cursor-pointer"
+            >
+              ...readmore
+            </strong>
+          </div>
+          <div className="pt-1 pb-4">
+            <Separator orientation="horizontal" />
+          </div>
+          <div className="flex gap-4">
+            <Link href={`${data.githubUrl}`} target="_blank">
+              <button className="py-2 rounded-sm px-4 text-slate-700 border border-[#C778DD] hover:bg-[#C778DD33] duration-150">
+                <Github className="text-2xl" />
+              </button>
+            </Link>
+            <Link href={`${data.viewUrl}`} target="_blank">
+              <button className="py-2 rounded-sm flex gap-2 px-4 text-slate-700 border border-[#C778DD] hover:bg-[#C778DD33] duration-150">
+                Live <ExternalLink className="text-2xl" />
+              </button>
+            </Link>
           </div>
         </div>
       </div>
