babb505a44352569f6528b18054f7957b3af0a80
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-12-31 17:46:04 +0545

    feedback:-Widget-added

---
+++ components/feedback-widget/widget.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Popover } from '@headlessui/react';
+import { MessageCircle } from 'lucide-react';
+
+import { WidgetForm } from './widget-form';
+
+export const Widget = () => {
+  return (
+    <Popover className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+
+      <Popover.Button className="bg-green-300 outline-none rounded-full px-3 h-12 text-slate-700 flex items-center group">
+        <MessageCircle className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear text-slate-700">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+};
