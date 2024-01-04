cff2f9ee61e065c4b4c1322487a3208146267e85
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2024-01-05 01:24:09 +0545

    skill-card:conditional-loader-added

--- app/(client)/(routes)/screen-cards/skill-card.tsx
+++ app/(client)/(routes)/screen-cards/skill-card.tsx
@@ -2,6 +2,9 @@
 
 import Image from 'next/image';
 import { SafeSkill, SafeUser } from '@/app/types';
+import { Loader } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import Container from '@/components/container';
 
 interface SkillCardProps {
   data: SafeSkill;
@@ -9,24 +12,38 @@ interface SkillCardProps {
 }
 
 const SkillCard: React.FC<SkillCardProps> = ({ data }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 1 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
-      <div className="col-span-1 cursor-pointer group">
-        <div className="flex flex-col gap-2 w-[6rem] ml-5 mr-5 pt-6 text-center">
-          <div
-            className="
-            aspect-square 
-            w-[6rem] 
-            relative 
-            overflow-hidden 
-            gap-2
-            rounded-xl
-          "
-          >
-            <Image
-              height="900"
-              width="900"
+      {isLoading ? (
+        <Container>
+          <Loader className="w-8 h-8 animate-spin" />
+        </Container>
+      ) : (
+        <div className="col-span-1 cursor-pointer group">
+          <div className="flex flex-col gap-2 w-[6rem] ml-5 mr-5 pt-6 text-center">
+            <div
               className="
+             aspect-square 
+             w-[6rem] 
+             relative 
+             overflow-hidden 
+             gap-2
+             rounded-xl
+             "
+            >
+              <Image
+                height="900"
+                width="900"
+                className="
                 object-cover 
                 h-[6rem] 
                 w-[6rem] 
@@ -36,15 +53,16 @@ const SkillCard: React.FC<SkillCardProps> = ({ data }) => {
                 rounded-md
                 transition           
             "
-              src={data.imageUrl}
-              alt="Listing"
-            />
-          </div>
-          <div className="text-center text-zinc-700 dark:text-white text-sm font-serif">
-            {data.label}
+                src={data.imageUrl}
+                alt="Listing"
+              />
+            </div>
+            <div className="text-center text-zinc-700 dark:text-white text-sm font-serif">
+              {data.label}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
