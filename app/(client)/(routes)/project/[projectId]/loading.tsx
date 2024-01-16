8782c8c329f9c608faadfa69032dabc3f4894252
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2024-01-16 18:46:58 +0545

    skeleton-loading:-mt-fixed

--- app/(client)/(routes)/project/[projectId]/loading.tsx
+++ app/(client)/(routes)/project/[projectId]/loading.tsx
@@ -7,7 +7,7 @@ const Loading = () => {
   return (
     <Container>
       <div className="w-full h-full p-10 duration-500">
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-4">
+        <div className="mt-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-4">
           <Skeleton className="h-4 p-4 w-[250px]" />
           <Skeleton className="w-full aspect-square rounded-xl md:aspect-[3.2/1]" />
           <Skeleton className="w-full aspect-square rounded-xl md:aspect-[3.2/1]" />
--- app/(client)/(routes)/resume/loading.tsx
+++ app/(client)/(routes)/resume/loading.tsx
@@ -7,7 +7,7 @@ const Loading = () => {
   return (
     <Container>
       <div className="w-full h-full p-10 duration-400">
-        <div className="mt-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-4">
+        <div className="mt-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-4">
           <Skeleton className="h-4 p-3 w-[550px]" />
           <Skeleton className="h-4 p-5 w-[250px]" />
           <Skeleton className="w-full aspect-square rounded-md md:aspect-[3.2/1]" />
