c76961b21f236337fe22b87489ae1ab79b2351c6
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2024-01-17 18:28:29 +0545

    small-updates-on:logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,14 +2,17 @@
 
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
 
 const MotionLink = motion(Link);
 
 const Logo = () => {
+  const router = useRouter();
   return (
     <div className="flex-row cursor-pointer">
       <MotionLink
         href="/"
+        onClick={() => router.refresh()}
         className="w-10 h-10 pl-[7px] bg-black text-white flex items-center rounded-full text-base font-bold"
         whileHover={{
           scale: 1.1,
